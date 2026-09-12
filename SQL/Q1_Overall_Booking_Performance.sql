d13c4df6a7ab425ba44366763fd243f9ebfb68b0
Anjali Rawat <rawatanjali0513@gmail.com>
2026-09-13 01:47:16 +0530

    Update Q1_Overall_Booking_Performance.sql

--- SQL/Q1_Overall_Booking_Performance.sql
+++ SQL/Q1_Overall_Booking_Performance.sql
@@ -1,4 +1,4 @@
---What is the company's overall booking performance and success rate?
+--Q1 What is the company's overall booking performance and success rate?
 --Analyze total bookings, successful rides, cancellations, driver-not-found bookings,
 -- success rate, failure rate, 
 --total booking value, and average booking value. what to do
@@ -62,5 +62,57 @@ FROM rides
 --Average Booking Value
 SELECT ROUND(AVG(booking_value),2) AS Avg_Booking_Value
 FROM rides
+
+
+-- Q2: Booking Value at Risk from Unsuccessful Rides
+
+-- -- Business Question: 
+-- How much booking value is potentially at risk because bookings 
+-- do not become successful rides? 
+
+-- -- Objective: 
+-- Compare successful and unsuccessful bookings and quantify the 
+-- booking-value exposure associated with unsuccessful rides. 
+
+-- 1. Successful Bookings 
+-- Find successful booking volume, total booking value, 
+-- and average booking value.
+
+SELECT 
+	booking_status,
+	COUNT(*) AS Successfull_booking,
+	ROUND(SUM(booking_value),2) AS total_booking_value,
+	ROUND(AVG(booking_value),2) AS Avg_booking_value
+FROM rides
+WHERE booking_status ='Success'
+GROUP BY booking_status
+
+-- 2. Unsuccessful Bookings
+-- Find total unsuccessful bookings, their booking value, 
+-- average booking value, and their share of total booking value.
+
+SELECT 
+	COUNT(*) AS UnSuccessfull_booking,
+	ROUND(SUM(booking_value),2) AS total_booking_value,
+	ROUND(AVG(booking_value),2) AS Avg_booking_value,
+	ROUND(
+	    SUM(booking_value)*100.0/
+	    (SELECT SUM(booking_value)FROM rides)
+	    ,2)AS UnSuccessful_Booking_Value_Percent 
+FROM rides
+WHERE booking_status<>'Success'
+
+
+-- 3. Unsuccessful Booking Breakdown 
+-- Compare booking volume and booking value across 
+-- different unsuccessful booking types.
+
+SELECT booking_status,
+	COUNT(*) AS Unsuccessfull_booking,
+	ROUND(SUM(booking_value),2) AS Total_booking_value
+FROM rides
+WHERE booking_status <>'Success'
+GROUP BY booking_status
+
 
 
